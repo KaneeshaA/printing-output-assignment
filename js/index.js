7cde8150127e2eaf2d187249a2b7d1cc1d7e3312
KaneeshaA <mmbo0001@algonquinlive.com>
2025-05-13 10:14:00 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+// Task 1
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = 
+`
+let a = 3;
+let b = 5;
+let c;
+
+------------------------
+
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c  = ${!a && !c}
+!a || !c  = ${!a || !c}
+`
+
+console.log(output)
+
+// Task 2
+
+// Declare variables
+let first_name = "Kaneesha";
+let last_name = "Andeso";
+let email = "mmbo0001@algonquinlive.com";
+
+// Create the expression using template literals
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+// Print the output in the browser's console
+console.log(output)
