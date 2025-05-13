058c90262069b19fa07c62486bf014f58924fc18
KaneeshaA <mmbo0001@algonquinlive.com>
2025-05-13 10:44:42 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,13 +35,10 @@ console.log(output)
 
 // Task 2
 
-// Declare variables
 let first_name = "Kaneesha";
 let last_name = "Andeso";
 let email = "mmbo0001@algonquinlive.com";
 
-// Create the expression using template literals
-let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
 
-// Print the output in the browser's console
-console.log(output)
+console.log(output);
